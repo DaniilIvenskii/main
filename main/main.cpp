25c15abc135d3b190a8c25071154d82adefdd0a7
vladislav <vladislavalekseev655@gmail.com>
2025-02-27 17:42:34 +0300

    HEhe

--- main/main.cpp
+++ main/main.cpp
@@ -1,40 +1,6 @@
-﻿
-
-#include <iostream>
+﻿#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
-    asd
-        a
-        sd
-        a
-        sd
-        f
-        ds
-
-        a
-        s
-        c
-        vs
-        asf
-
-        sa
-
-        as
-
-        das
-        das
-        d
-        a
-        das
-        das
-
-        das
-        d
-        as
-        das
-        da
-
-        d
+    std::cout << "Hello WOW!\n";
 }
